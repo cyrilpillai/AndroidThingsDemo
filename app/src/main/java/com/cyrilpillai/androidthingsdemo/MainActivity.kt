20fb629803ff80dabad2ea8d59876a780fa162d4
cyrilpillai <cyrilpillai@gmail.com>
2018-01-07 17:45:57 +0530

    Initial Commit

---
+++ app/src/main/java/com/cyrilpillai/androidthingsdemo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.cyrilpillai.androidthingsdemo
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by cyril on 7/1/18.
+ */
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
